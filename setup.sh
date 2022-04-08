bd114184180f092418b37be9aa97a6d1d33db0ed
mmqmzk <mmqmzk@gmail.com>
2022-04-08 17:32:16 +0800

    fix termux install

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -xe
+
 DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
-pushd "$DIR"
+cd "$DIR"
 if [[ ! -e ranger ]]; then
   git clone git@github.com:mmqmzk/ranger.git || git clone https://github.com/mmqmzk/ranger.git
 fi
-pushd ranger
+cd ranger
+if [[ "$*" == *termux* ]]; then
+  git checkout termux
+fi
 eval "$(sed -n '0,/```/d;/```/q;p' README.md)"
-popd
-popd
